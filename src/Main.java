5bcb544f6c96827f4e179889a78f3323cd4e329a
Jade Trubert <jade_gurgel@outlook.com>
2025-04-26 19:00:05 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        //Disciplina d[] = new Disciplina [3];
-
-        // d[0] = new Disciplina(123456, "POO", 1);
-        // d[1] = new Disciplina(123457, "Raciocínio Lógico", 1);
-        // d[2] = new Disciplina(123458, "Matemática para computação", 1);
-
-        // Regular r[] = new Regular[2];
-        // r[0] = new Regular(60715479, "Ana Claudia", 24, 154879);
-        // r[1] = new Regular(60715452, "Juli", 22, 154877);
-
-        // Bolsista b[] = new Bolsista[1];
-        //b[0] = new Bolsista(60458974, "Fernando", 29, 215874);
-
-        // Professor p[] = new Professor[1];
-        // p[0] = new Professor(415789, "Dra Constantine", 45, "CCT");
-
-        // Visitante v[] = new Visitante[2];
-        // v[0] = new Visitante(484579, "Jonas da Silva", 34);
-        // v[1] = new Visitante(484565, "Jana da Silva", 31);
-
-        //########################################################################
-
         Disciplina d1 = new Disciplina(123456, "POO", 1);
         Disciplina d2 = new Disciplina(123457, "Raciocínio Lógico", 1);
         Disciplina d3 = new Disciplina(123458, "Matemática para Computação", 1);
